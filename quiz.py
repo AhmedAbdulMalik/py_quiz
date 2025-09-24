c4ec43bcb73387baf8236951eb4c4ba4b043b4f2
AhmedAbdulMalik <abdulmalikandanas@gmail.com>
2025-09-25 02:49:41 +0530

    refactor:added a list of dicts

--- quiz.py
+++ quiz.py
@@ -1,15 +1,25 @@
-q1 = "who is the most creepist Tech CEO?"
-op1 = "[a]:Mark zuckerberg"
-op2 = "[b]:Bill gates"
-op3 = "[c]:Elon Musk"
-op4 = "[d]:Larry Ellison"
-a1 = "a"
 
-answer = input(f"{q1}\n{op1}\n{op2}\n{op3}\n{op4}\n")
-answer = answer.lower()
-
-if answer==a1:
-	print("Correct")
-else:
-	print("wrong")
-
+quiz = [
+	 {
+	 "question":"who is the most creepiest Tech CEO?",
+		"options": 
+		{
+		 "a": "Mark zuckerberg",
+		 "b":Bill gates",
+		 "c":Elon Musk",
+		 "d":Larry Ellison"
+		},
+		"answer": "a"
+	 },
+	 {
+	"question":"Which AI company violates International Copyright laws?",
+		"options":
+		{
+		"a":"ClosedAI",
+		"b":"Entropic",
+		"c":"Co-tear",
+		"d":"All of the above"},
+		"answer":"d"
+	 }
+       ]
+	
