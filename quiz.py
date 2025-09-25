41e9c18532b382c5e4d9ecefefbca9ef3055f29e
AhmedAbdulMalik <abdulmalikandanas@gmail.com>
2025-09-26 03:48:42 +0530

    added game loop :)

--- quiz.py
+++ quiz.py
@@ -5,9 +5,9 @@ quiz = [
 		"options": 
 		{
 		 "a": "Mark zuckerberg",
-		 "b":Bill gates",
-		 "c":Elon Musk",
-		 "d":Larry Ellison"
+		 "b": "Bill gates",
+		 "c": "Elon Musk",
+		 "d": "Larry Ellison"
 		},
 		"answer": "a"
 	 },
@@ -15,11 +15,16 @@ quiz = [
 	"question":"Which AI company violates International Copyright laws?",
 		"options":
 		{
-		"a":"ClosedAI",
-		"b":"Entropic",
-		"c":"Co-tear",
-		"d":"All of the above"},
+		"a": "ClosedAI",
+		"b": "Entropic",
+		"c": "Co-tear",
+		"d": "All of the above"},
 		"answer":"d"
 	 }
        ]
-	
+for plays in quiz:
+	print(plays["question"])
+	for key,value in plays["options"].items():
+		print(f"[ {key} ] : {value}")
+#since optiond are stored in a dict we will use key & value
+	user_answer = input("Answer:").lower()		
